2fb9255057deae43d0bcbcd720b8f1daec7fed17
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2024-01-24 12:30:53 -0500

    updates target

--- projects/SensoriMotorPrediction/Smp1/make_target.py
+++ projects/SensoriMotorPrediction/Smp1/make_target.py
@@ -7,14 +7,16 @@ nblocks = 10
 for block in range(nblocks):
     tgt = pd.read_csv('target/template.tgt', sep="\t")
 
-    st = tgt.startTime
+    st = tgt.startTimeDiff
     st1 = st.iloc[[0]]
     st_rest = st.iloc[1:]
     st_rest = st_rest.sample(frac=1).reset_index(drop=True)
     st = pd.concat([st1, st_rest]).reset_index(drop=True)
-
+    for s, d in enumerate(st.iloc[1:]):
+        st.iloc[[s+1]] = d + st.iloc[[s]]
     tgt = tgt.sample(frac=1).reset_index(drop=True)
-    tgt.startTime = st
+    tgt = tgt.drop('startTimeDiff', axis=1)
+    tgt['startTime'] = st
 
     filename = f"{experiment}_{subj}_{block + 1:02}.tgt"
 

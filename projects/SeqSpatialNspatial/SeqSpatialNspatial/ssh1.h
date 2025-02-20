18154deb0a5dad997deb4156a8dadbaefb4c43ce
Amin <alighavam79@gmail.com>
2025-02-20 15:00:57 -0500

    Armin: number of max presses changed

--- projects/SeqSpatialNspatial/SeqSpatialNspatial/ssh1.h
+++ projects/SeqSpatialNspatial/SeqSpatialNspatial/ssh1.h
@@ -27,7 +27,7 @@ using namespace std;
 #define RECORDRATE 2    // recording update rate
 #define UPDATE_TEXTDISP 60
 #define SCR_SCALE 1.84/72 //3/72 //2.54/72 // cm/pixel 
-#define MAX_PRESS 5 // edited by SKim, fMRI
+#define MAX_PRESS 2 // edited by Armin
 ///////////////////////////////////////////////////////////////
 // Enumeration of Trial State 
 ///////////////////////////////////////////////////////////////

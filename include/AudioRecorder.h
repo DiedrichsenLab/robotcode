830af4349b54e78f4004ac2881b33cab8d4974b1
Amin <53136447+alighavam@users.noreply.github.com>
2026-03-15 15:27:56 -0400

    sampling rates updated

--- include/AudioRecorder.h
+++ include/AudioRecorder.h
@@ -10,6 +10,7 @@ public:
     ~AudioRecorder();
 
     bool start(const std::string& wavPath, int sampleRate = 16000, int channels = 1, int bitsPerSample = 16);
+    //bool start(const std::string& wavPath, int sampleRate = 8000, int channels = 1, int bitsPerSample = 16);
     //bool start(const std::string& wavPath, int sampleRate = 4000, int channels = 1, int bitsPerSample = 16);
 
     void stop();
